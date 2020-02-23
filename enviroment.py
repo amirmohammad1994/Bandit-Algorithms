4eaac328ada9825c1ce117d052fd81ed73c8662c
Amir Ohadi <amirmohammadohadi@gmail.com>
2020-02-23 16:29:15 +0330

    delete env

--- enviroment.py
+++
@@ -1,8 +0,0 @@
-class Enviroment:
-    def make(env_name):
-        if env_name == 'contextual':
-            return
-        elif env_name == 'simple':
-            return
-        elif env_name == 'random-graph':
-            return
