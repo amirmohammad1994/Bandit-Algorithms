db4633d7adb2dc80121d02824ef82458aa76f31e
Amir Ohadi <amirmohammadohadi@gmail.com>
2020-02-23 19:02:02 +0330

    env added.

---
+++ enviroment.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from bandit import Bandit
+from explore_then_exploit_agent import ExploreThenExploit
+from MC_simulator import *
+from epsilon_greedy_agent import EpsilonGreedy
+from ubc1_agent import UBC1Agent
+from report import plot
+
